45a8036dbbd9a2a54a4d046d24b2fa66cc8134de
Klien-m <1203249465@qq.com>
2020-07-18 16:57:42 +0800

    修改了下“矩阵中的路径”

--- src/offer_java/HasPath.java
+++ src/offer_java/HasPath.java
@@ -2,53 +2,35 @@ package offer_java;
 
 /**
  * @since 2020/7/14 20:40
- *
+ * <p>
  * 矩阵中的路径
  * 请设计一个函数，用来判断在一个矩阵中是否存在一条包含某字符串所有字符的路径。
  * 路径可以从矩阵中的任意一个格子开始，每一步可以在矩阵中向左，向右，向上，向下移动一个格子。
  * 如果一条路径经过了矩阵中的某一个格子，则该路径不能再进入该格子。
- *
+ * <p>
  * 知识点：DFS
  */
 public class HasPath {
-    char[] mat;
-    int h = 0, w = 0;
-    int strLength = 0;
-    int[] dir = {-1, 0, 1, 0, -1};
 
-    private boolean dfs(int i, int j, int pos, char[] str) {
-        if (i < 0 || i >= h || j < 0 || j >= w) {
-            return false;
-        }
-        char ch = mat[i * w + j];
-        if (ch == '#' || ch != str[pos]) {
-            return false;
-        }
-        if (pos + 1 == strLength) {
-            return true;
-        }
-        mat[i * w + j] = '#';
-        for (int k = 0; k < 4; ++k) {
-            if (dfs(i + dir[k], j + dir[k + 1], pos + 1, str)) {
-                return true;
+    // hasPath
+    public boolean exist(char[][] board, String word) {
+        char[] words = word.toCharArray();
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                if (dfs(board, words, i, j, 0)) return true;
             }
         }
-        mat[i * w + j] = ch;
         return false;
     }
 
-    public boolean hasPath(char[] matrix, int rows, int cols, char[] str) {
-        mat = matrix;
-        h = rows;
-        w = cols;
-        strLength = str.length;
-        for (int i = 0; i < rows; ++i) {
-            for (int j = 0; j < cols; ++j) {
-                if (dfs(i, j, 0, str)) {
-                    return true;
-                }
-            }
-        }
-        return false;
+    boolean dfs(char[][] board, char[] word, int i, int j, int k) {
+        if (i >= board.length || i < 0 || j >= board[0].length || j < 0 || board[i][j] != word[k]) return false;
+        if (k == word.length - 1) return true;
+        char tmp = board[i][j];
+        board[i][j] = '/';
+        boolean res = dfs(board, word, i + 1, j, k + 1) || dfs(board, word, i - 1, j, k + 1) ||
+                dfs(board, word, i, j + 1, k + 1) || dfs(board, word, i, j - 1, k + 1);
+        board[i][j] = tmp;
+        return res;
     }
 }
